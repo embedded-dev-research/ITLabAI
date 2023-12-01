25b67cf8f197ea17b10fd12342e70b88b1929174
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2023-12-01 19:00:40 +0300

    Implement graph data structure

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+#include "Graph.hpp"
 #include "gtest/gtest.h"
 
 TEST(basic, basic_test) {
@@ -10,4 +11,37 @@ TEST(basic, basic_test) {
 
   // Assert
   ASSERT_EQ(5, c);
+}
+TEST(graph, basic_test1) {
+Graph graph(5);
+graph.addEdge(0, 1);
+graph.addEdge(0, 4);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(1, 4);
+graph.addEdge(2, 3);
+graph.addEdge(3, 4);
+ASSERT_EQ(graph.checkconnect(0, 1), 1);
+}
+TEST(graph, basic_test2) {
+Graph graph(5);
+graph.addEdge(0, 1);
+graph.addEdge(0, 4);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(1, 4);
+graph.addEdge(2, 3);
+graph.addEdge(3, 4);
+ASSERT_EQ(graph.checkconnect(0, 2), 0);
+}
+TEST(graph, basic_test3) {
+Graph graph(5);
+graph.addEdge(0, 1);
+graph.addEdge(0, 4);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(1, 4);
+graph.addEdge(2, 3);
+graph.addEdge(3, 4);
+ASSERT_EQ(graph.checkconnect(1, 1), 0);
 }
