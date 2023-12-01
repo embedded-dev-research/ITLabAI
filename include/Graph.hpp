eed490854c7e62a396f6eced396e5c1bcedd960c
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2023-12-01 18:31:50 +0300

    Implement graph data structure

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -4,17 +4,14 @@
 
 class Node {
  public:
-    int abc;
-    std::vector<int> ConnectedNode;
-
+  int abc;
+  std::vector<int> ConnectedNode;
   Node(int a = 0) { 
       abc = a;
   }
-
   void addconnect(int connectedGraph) {
     ConnectedNode.push_back(connectedGraph);
   }
-
   std::vector<int> CheckConnectedNode() { 
       return ConnectedNode; 
   }
@@ -22,17 +19,14 @@ class Node {
 
 class Graph {
  public:
-    int V;
-    std::vector<Node> adjList;
-
+     int V;
+     std::vector<Node> adjList;
   Graph(int vertices) : V(vertices) {
       adjList.resize(V); 
   }
-
   void addEdge(int a, int b) { 
       adjList[a].addconnect(b); 
   }
-
   bool checkconnect(int a, int b) {
     std::vector<int> vec = adjList[a].CheckConnectedNode();
     auto it = std::find(vec.begin(), vec.end(), b);
@@ -43,14 +37,4 @@ class Graph {
       return false;
     }
   }
-
-  // void printGraph() {
-  //   for (int i = 0; i < V; ++i) {
-  //     std::cout << "Вершина " << i << " связана с вершинами: ";
-  //     for (int v : adjList[i]) {
-  //       std::cout << v << " ";
-  //     }
-  //     std::cout << std::endl;
-  //   }
-  // }
 };
