7ac5100de3acc0e3eac7a1364149d73ebae07705
NeiroYT <nekkk1.lnnn1@yandex.ru>
2023-12-02 14:47:18 +0300

    Timertest update

--- app/pref/timertest.cpp
+++ app/pref/timertest.cpp
@@ -7,61 +7,80 @@
 
 using namespace std;
 
-inline void counter1(int i, int n) {
+inline void counter1(int i, int n, double* arr) {
   clock_t timer;
+  double res;
   timer = clock();
   this_thread::sleep_for(chrono::milliseconds(n));
-  timer = clock() - timer;
-  cout << timer << endl;
+  res = (double)(clock() - timer) / CLOCKS_PER_SEC;
+  arr[i] = res;
 }
 
-inline void counter2(int i, int n) {
+inline void counter2(int i, int n, double* arr) {
   auto chronotimer = chrono::high_resolution_clock();
   auto duration = chrono::duration<double, milli>();
   auto start = chronotimer.now();
   this_thread::sleep_for(chrono::milliseconds(n));
   auto end = chronotimer.now();
   duration = end - start;
-  cout << duration.count() << endl;
+  arr[i] = duration.count();
 }
 
-inline void counter3(int i, int n) {
+inline void counter3(int i, int n, double* arr) {
   double duration;
   double start = omp_get_wtime();
   this_thread::sleep_for(chrono::milliseconds(n));
   double end = omp_get_wtime();
   duration = end - start;
-  cout << duration * 1000.0 << endl;
+  arr[i] = duration;
 }
 
 const int ITERS = 100;
 
 int main() {
   int n = 2000;
-  thread threads[100];
+  thread threads[ITERS];
+  double arr[ITERS];
   char choice;
   cin >> choice;
   this_thread::sleep_for(chrono::milliseconds(3000));
   if (choice == '1') {
     for (int i = 0; i < ITERS; i++) {
-      threads[i] = thread(counter1, i, n);
+      threads[i] = thread(counter1, i, n, arr);
     }
     for (int i = 0; i < ITERS; i++) {
       threads[i].join();
+    }
+    for (int i = 0; i < ITERS; i++) {
+      cout << i + 1 << ") " << arr[i] << endl;
     }
   } else if (choice == '2') {
     for (int i = 0; i < ITERS; i++) {
-      threads[i] = thread(counter2, i, n);
+      threads[i] = thread(counter2, i, n, arr);
     }
     for (int i = 0; i < ITERS; i++) {
       threads[i].join();
+    }
+    for (int i = 0; i < ITERS; i++) {
+      cout << i + 1 << ") " << arr[i] << endl;
     }
   } else if (choice == '3') {
+#pragma omp parallel for num_threads(ITERS)
     for (int i = 0; i < ITERS; i++) {
-      threads[i] = thread(counter2, i, n);
+      counter3(i, n, arr);
+    }
+    for (int i = 0; i < ITERS; i++) {
+      cout << i + 1 << ") " << arr[i] << endl;
+    }
+  } else if (choice == '4') {
+    for (int i = 0; i < ITERS; i++) {
+      threads[i] = thread(counter3, i, n, arr);
     }
     for (int i = 0; i < ITERS; i++) {
       threads[i].join();
+    }
+    for (int i = 0; i < ITERS; i++) {
+      cout << i + 1 << ") " << arr[i] << endl;
     }
   }
   return 0;
