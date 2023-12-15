2ae0de3aae8ba39380126829e5025c44475f12a5
Semyon1104 <sema_titov_04@mail.ru>
2023-12-15 19:50:31 +0300

    add src

---
+++ src/read/Reader_img.cpp
@@ -0,0 +1,18 @@
+﻿#include "Reader_img.hpp"
+using namespace std;
+using namespace cv;
+
+void read(std::string path)
+{
+  Mat image = imread(path);
+  if (image.empty())
+  {
+	throw "Could not open or find the image";
+	cin.get();
+  }
+  String windowName = "Image";
+  namedWindow(windowName);
+  imshow(windowName, image);
+  waitKey(0);
+  destroyWindow(windowName);
+}
