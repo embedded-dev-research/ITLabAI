295de9a7d8e3a667a35023c19ec3909819232cf0
NeiroYT <119765880+NeiroYT@users.noreply.github.com>
2023-12-30 12:13:33 +0300

    Implement throughput measurement function (#36)
    Solving #11

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <random>
 #include <thread>
+#include <vector>
 
 #include "graph/graph.hpp"
 #include "gtest/gtest.h"
@@ -423,6 +425,107 @@ TEST(accuracy, accuracy_norm_throws_when_bad_pointer) {
   EXPECT_ANY_THROW(accuracy_norm<double>(a, b, 5));
   EXPECT_ANY_THROW(accuracy_norm<double>(b, a, 5));
   delete[] b;
+}
+
+// ==========================
+
+// ==========================
+// Throughput tests
+
+template <typename T>
+std::vector<T> matrix_sum(const std::vector<T> &first,
+                          const std::vector<T> &second) {
+  std::vector<T> res(first.size());
+  std::transform(first.begin(), first.end(), second.begin(), res.begin(),
+                 std::plus<T>());
+  return res;
+}
+
+template <typename T>
+std::vector<T> matrix_mul(const size_t n, const std::vector<T> &first,
+                          const std::vector<T> &second) {
+  std::vector<T> mul(n * n, T(0));
+  for (size_t i = 0; i < n; i++) {
+    for (size_t k = 0; k < n; k++) {
+      for (size_t j = 0; j < n; j++) {
+        mul[n * i + j] += first[n * i + k] * second[n * k + j];
+      }
+    }
+  }
+  return mul;
+}
+
+TEST(throughput, matrix_operations_throughput_is_positive) {
+  size_t n = 200;
+  std::vector<int> a(n * n);
+  std::vector<int> b(n * n);
+  size_t ptr = 0;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      a[ptr] = i + j;
+      b[ptr] = i - j;
+      ptr++;
+    }
+  }
+  double tp;
+  tp = throughput<double, std::ratio<1, 1> >(matrix_sum<int>, a, b);
+  EXPECT_GE(tp, 0);
+  tp = throughput<double, std::ratio<1, 1> >(matrix_mul<int>, n, a, b);
+  EXPECT_GE(tp, 0);
+}
+TEST(throughput, matrix_operations_throughput_avg_is_positive) {
+  size_t n = 200;
+  std::vector<int> a(n * n);
+  std::vector<int> b(n * n);
+  size_t ptr = 0;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      a[ptr] = i + j;
+      b[ptr] = i - j;
+      ptr++;
+    }
+  }
+  double tp;
+  tp = throughput_avg<double, std::ratio<1, 1> >(10, matrix_sum<int>, a, b);
+  EXPECT_GE(tp, 0);
+  tp = throughput_avg<double, std::ratio<1, 1> >(10, matrix_mul<int>, n, a, b);
+  EXPECT_GE(tp, 0);
+}
+TEST(throughput, matrix_operations_throughput_omp_is_positive) {
+  size_t n = 200;
+  std::vector<int> a(n * n);
+  std::vector<int> b(n * n);
+  size_t ptr = 0;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      a[ptr] = i + j;
+      b[ptr] = i - j;
+      ptr++;
+    }
+  }
+  double tp;
+  tp = throughput_omp(matrix_sum<int>, a, b);
+  EXPECT_GE(tp, 0);
+  tp = throughput_omp(matrix_mul<int>, n, a, b);
+  EXPECT_GE(tp, 0);
+}
+TEST(throughput, matrix_operations_throughput_omp_avg_is_positive) {
+  size_t n = 200;
+  std::vector<int> a(n * n);
+  std::vector<int> b(n * n);
+  size_t ptr = 0;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      a[ptr] = i + j;
+      b[ptr] = i - j;
+      ptr++;
+    }
+  }
+  double tp;
+  tp = throughput_omp_avg(10, matrix_sum<int>, a, b);
+  EXPECT_GE(tp, 0);
+  tp = throughput_omp_avg(10, matrix_mul<int>, n, a, b);
+  EXPECT_GE(tp, 0);
 }
 
 // ==========================
