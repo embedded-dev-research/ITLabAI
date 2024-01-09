2a4a2a462c83d85fa214cf1b5cb5744b30de0943
Semyon1104 <sema_titov_04@mail.ru>
2024-01-09 22:33:40 +0300

    fix path in test_read

--- test/test_read/test_read.cpp
+++ test/test_read/test_read.cpp
@@ -2,12 +2,11 @@
 #include "read/reader_img.hpp"
 using namespace cv;
 TEST(Read_img, can_read_image) {
-  ASSERT_NO_THROW(Mat image = imread("../include/read/image.jpg"););
+  ASSERT_NO_THROW(Mat image = imread("../../include/read/image.jpg"););
 }
 TEST(Read_img, can_show_image) {
-  Mat image = imread("../include/read/image.jpg");
+  Mat image = imread("../../include/read/image.jpg");
   String windowName = "Image";
   namedWindow(windowName);
-  imshow(windowName, image);
   ASSERT_NO_THROW(imshow(windowName, image););
 }
