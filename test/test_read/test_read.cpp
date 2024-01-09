df12bea3f3be59090d9486ec22069f937e441a39
Semyon1104 <sema_titov_04@mail.ru>
2024-01-09 21:46:38 +0300

    add download img

--- test/test_read/test_read.cpp
+++ test/test_read/test_read.cpp
@@ -2,10 +2,10 @@
 #include "read/reader_img.hpp"
 using namespace cv;
 TEST(Read_img, can_read_image) {
-  ASSERT_NO_THROW(Mat image = imread("image.jpg"););
+  ASSERT_NO_THROW(Mat image = imread("../include/read/image.jpg"););
 }
 TEST(Read_img, can_show_image) {
-  Mat image = imread("image.jpg");
+  Mat image = imread("../include/read/image.jpg");
   String windowName = "Image";
   namedWindow(windowName);
   imshow(windowName, image);
