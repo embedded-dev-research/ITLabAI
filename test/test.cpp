65b30ac1ac238870e1a5c353cd3992b216fbcec0
NeiroYT <nekkk1.lnnn1@yandex.ru>
2024-01-12 15:10:39 +0300

    Tanh

--- test/test.cpp
+++ test/test.cpp
@@ -253,6 +253,11 @@ T sin(const T &elem) {
   return std::sin(elem);
 }
 
+template<typename T>
+T tanh(const T &elem) {
+  return std::tanh(elem);
+}
+
 TEST(ewlayer, works_with_minus) {
   EWLayer<double> layer({2, 2}, minus<double>);
   std::vector<double> input = {2.0, 3.9, 0.1, 2.3};
@@ -267,7 +272,8 @@ TEST(ewlayer, works_with_sin) {
   EWLayer<double> layer({2, 2}, sin<double>);
   std::vector<double> input = {2.0, 3.9, 0.1, 2.3};
   std::vector<double> converted_input(4);
-  std::transform(input.begin(), input.end(), converted_input.begin(), sin<double>);
+  std::transform(input.begin(), input.end(), converted_input.begin(),
+                 sin<double>);
   std::vector<double> output = layer.run(input);
   for (size_t i = 0; i < 4; i++) {
     EXPECT_NEAR(output[i], converted_input[i], 1e-5);
@@ -278,6 +284,18 @@ TEST(ewlayer, relu_test) {
   EWLayer<double> layer({2, 2}, relu<double>);
   std::vector<double> input = {1.0, -1.0, 2.0, -2.0};
   std::vector<double> converted_input = {1.0, 0.0, 2.0, 0.0};
+  std::vector<double> output = layer.run(input);
+  for (size_t i = 0; i < 4; i++) {
+    EXPECT_NEAR(output[i], converted_input[i], 1e-5);
+  }
+}
+
+TEST(ewlayer, tanh_test) {
+  EWLayer<double> layer({2, 2}, tanh<double>);
+  std::vector<double> input = {1.0, -1.0, 2.0, -2.0};
+  std::vector<double> converted_input(4);
+  std::transform(input.begin(), input.end(), converted_input.begin(),
+                 tanh<double>);
   std::vector<double> output = layer.run(input);
   for (size_t i = 0; i < 4; i++) {
     EXPECT_NEAR(output[i], converted_input[i], 1e-5);
