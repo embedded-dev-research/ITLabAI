0b3b267344a582a04cf8557a7a9df59d4ab2c9d6
Semyon1104 <sema_titov_04@mail.ru>
2024-02-02 19:05:23 +0300

    show -> write

--- test/test_read/test_read.cpp
+++ test/test_read/test_read.cpp
@@ -4,9 +4,8 @@ using namespace cv;
 TEST(Read_img, can_read_image) {
   ASSERT_NO_THROW(Mat image = imread("image.jpg"););
 }
-TEST(Read_img, can_show_image) {
+TEST(Read_img, can_save_image) {
   Mat image = imread("image.jpg");
-  String windowName = "Image";
-  namedWindow(windowName);
-  ASSERT_NO_THROW(imshow(windowName, image););
+  String outputFileName = "output_image.jpg";
+  ASSERT_NO_THROW(imwrite(outputFileName, image););
 }
