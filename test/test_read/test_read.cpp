ccdaca935ad8b5328265bc9feaf447207461b200
Semyon1104 <sema_titov_04@mail.ru>
2024-02-17 14:09:34 +0300

    one exe for all test

--- test/test_read/test_read.cpp
+++ test/test_read/test_read.cpp
@@ -2,10 +2,10 @@
 #include "read/reader_img.hpp"
 using namespace cv;
 TEST(Read_img, can_read_image) {
-  ASSERT_NO_THROW(Mat image = imread("image.jpg"););
+  ASSERT_NO_THROW(Mat image = imread("../image.jpg"););
 }
 TEST(Read_img, can_save_image) {
-  Mat image = imread("image.jpg");
+  Mat image = imread("../image.jpg");
   String output_file_name = "output_image.jpg";
   ASSERT_NO_THROW(imwrite(output_file_name, image););
 }
