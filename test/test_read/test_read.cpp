112098a590ae339e373160eac7fcee4bca090714
Semyon1104 <sema_titov_04@mail.ru>
2024-02-17 16:18:14 +0300

    for clang-tidy

--- test/test_read/test_read.cpp
+++ test/test_read/test_read.cpp
@@ -9,9 +9,8 @@ std::string getImagePath() {
   char* workspace = getenv("GITHUB_WORKSPACE");
   if (workspace != nullptr) {
     return std::string(workspace) + "build/test/image.jpg";
-  } else {
-    return "../image.jpg";
   }
+  return "../image.jpg";
 }
 TEST(Read_img, can_read_image) {
   ASSERT_NO_THROW(Mat image = imread(getImagePath()););
