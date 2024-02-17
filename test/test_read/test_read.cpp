99afc6091f15339429204751d3218595f558a5fc
Semyon1104 <sema_titov_04@mail.ru>
2024-02-17 14:50:32 +0300

    add function to find path to GITHUB and local machine

--- test/test_read/test_read.cpp
+++ test/test_read/test_read.cpp
@@ -1,11 +1,22 @@
 #include "gtest/gtest.h"
 #include "read/reader_img.hpp"
+#include <cstdlib>
+#include <string>
 using namespace cv;
+
+std::string getImagePath() {
+  char* workspace = getenv("GITHUB_WORKSPACE");
+  if (workspace != nullptr) {
+    return std::string(workspace) + "build/test/image.jpg";
+  } else {
+    return "../image.jpg";
+  }
+}
 TEST(Read_img, can_read_image) {
-  ASSERT_NO_THROW(Mat image = imread("../image.jpg"););
+  ASSERT_NO_THROW(Mat image = imread(getImagePath()););
 }
 TEST(Read_img, can_save_image) {
-  Mat image = imread("../image.jpg");
+  Mat image = imread(getImagePath());
   String output_file_name = "output_image.jpg";
   ASSERT_NO_THROW(imwrite(output_file_name, image););
 }
