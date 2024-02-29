f53109bc9d3d1d7c05c675ba21ecbfacaef311b7
SuvorovDm-1 <dim-suv@mail.ru>
2024-02-29 12:04:12 +0300

    first_prot_typed_tensor

--- include/layers/Tensor.hpp
+++ include/layers/Tensor.hpp
@@ -2,24 +2,53 @@
 
 #include <iostream>
 #include <vector>
+#include <type_traits>
 
 #include "Layer.hpp"
+
+enum Type {
+  INT = 0,
+  DOUBLE,
+};
+
+template <typename T>
+Type GetTypeEnum() {
+  if (std::is_same(T, int)::value)
+    return INT;
+  if (std::is_same(T, int)::value)
+    return DOUBLE;
+}
 
 class Tensor {
 private:
   Shape size_;
-  std::vector<double> values_;
+  std::vector<uint8_t> values_;
+  Type type_;
+
+  std::vector<uint8_t> SetRightTypeValues();
+
+  template <typename T>
+  std::vector<T>* as();
 public:
-  Tensor() = default;
-  Tensor(const size_t dims_count) : size_(dims_count) {values_ = {};};
-  Tensor(const std::vector<size_t>& dims) : size_(dims) {values_ = {};};
-  Tensor(const Shape& sh) : size_(sh), values_(sh.count(), 0) {};
-  Tensor(const Shape& sh, const std::vector<double>& values);
+  Tensor(const size_t dims_count, Type type) : size_(dims_count) {
+    type_ = type;
+    SetRightTypeValues();
+  };
+  Tensor(const std::vector<size_t>& dims, Type type) : size_(dims) {
+    type_ = type;
+    SetRightTypeValues();
+  }
+  Tensor(const Shape& sh, Type type) : size_(sh) {
+    type_ = type;
+    SetRightTypeValues();
+  }
   Tensor(const Tensor& t) = default;
 
   Shape get_size() const { return size_; }
 
+  template <typename T>
   double& operator()(const std::vector<size_t>& coords); // write
+  template <typename T>
   double operator()(const std::vector<size_t>& coords) const; // read
 
   friend std::ostream& operator<<(std::ostream& out, const Tensor& t);
--- src/layers/Tensor.cpp
+++ src/layers/Tensor.cpp
@@ -7,22 +7,43 @@
 #include "layers/Layer.hpp"
 #include "Tensor.hpp"
 
-Tensor::Tensor(const Shape& sh, const std::vector<double>& values) {
-  if (values.size() != sh.count())
-    throw std::invalid_argument("vector length mismatch with shape");
-  size_ = sh;
-  values_ = values;
+std::vector<uint8_t> Tensor::SetRightTypeValues() {
+  if (type_ == INT) {
+    return std::vector<uint8_t>(size_.count() * sizeof(int), 0);
+  }
+  if (type_ == DOUBLE) {
+    return std::vector<uint8_t>(size_.count() * sizeof(double), 0.0);
+  }
 }
 
+template <typename T>
+std::vector<T>* Tensor::as() {
+  if (GetTypeEnum<T>() == type_){
+    return reinterpret_cast<std::vector<T>*>(&values_);
+  }
+  return nullptr;
+}
+
+template <typename T>
 double& Tensor::operator()(const std::vector<size_t>& coords) {
   size_t s = size_.get_index(coords);
-  return values_[s];
+  std::vector<T>* res_vector = this->as<T>();
+  if (res_vector == nullptr) {
+    throw std::exception("invalid type\n");
+  }
+  return (*res_vector)[s];
 } // write
 
+template <typename T>
 double Tensor::operator()(const std::vector<size_t>& coords) const {
   size_t s = size_.get_index(coords);
-  return values_[s];
+  std::vector<T>* res_vector = this->as<T>();
+  if (res_vector == nullptr) {
+    throw std::exception("invalid type\n");
+  }
+  return (*res_vector)[s];
 } // read
+
 
 std::ostream& operator<<(std::ostream& out, const Tensor& t) {
     for (int i = 0; i < t.size_.count(); i++) {
@@ -35,14 +56,14 @@ std::ostream& operator<<(std::ostream& out, const Tensor& t) {
     return out;
 }
 
-Tensor initial_square_picture() {
+Tensor initial_square_int_picture() {
     srand(time(nullptr));
     std::vector<size_t> initial_size = {224, 224};
-    Tensor picture(initial_size);
+    Tensor picture(initial_size, INT);
 
     for (size_t h = 0; h < picture.get_size()[0]; h++) {
       for (size_t w = 0; w < picture.get_size()[1]; w++) {
-        picture({h, w}) = rand() % 255;
+        picture.operator()<int>({h, w}) = rand() % 255;
       }
     }
 
