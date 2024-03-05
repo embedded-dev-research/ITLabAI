428e5db5bede1dead300f1b5220f85decde443f2
Semyon1104 <129722895+Semyon1104@users.noreply.github.com>
2024-03-05 22:03:42 +0300

    Update alexnet_src.cpp

--- src/alexnet/alexnet_src.cpp
+++ src/alexnet/alexnet_src.cpp
@@ -7,7 +7,7 @@ void AlexNetSample(std::string& path) {
   TF_Graph* graph = TF_NewGraph();
   TF_Status* status = TF_NewStatus();
   TF_SessionOptions* session_options = TF_NewSessionOptions();
-  TF_Session* session = TF_NewSession(graph, sessionOptions, status);
+  TF_Session* session = TF_NewSession(graph, session_options, status);
 
   if (TF_GetCode(status) != TF_OK) {
     std::cerr << "Error creating TensorFlow session: " << TF_Message(status)
@@ -23,16 +23,16 @@ void AlexNetSample(std::string& path) {
   size_t file_size = file.tellg();
   file.seekg(0, std::ios::beg);
 
-  std::vector<char> buffer(fileSize);
-  file.read(buffer.data(), fileSize);
+  std::vector<char> buffer(file_size);
+  file.read(buffer.data(), file_size);
   file.close();
 
   TF_Buffer* model_buffer =
       TF_NewBufferFromString(buffer.data(), buffer.size());
   TF_ImportGraphDefOptions* import_options = TF_NewImportGraphDefOptions();
-  TF_GraphImportGraphDef(graph, modelBuffer, importOptions, status);
-  TF_DeleteImportGraphDefOptions(importOptions);
-  TF_DeleteBuffer(modelBuffer);
+  TF_GraphImportGraphDef(graph, model_buffer, import_options, status);
+  TF_DeleteImportGraphDefOptions(import_options);
+  TF_DeleteBuffer(model_buffer);
 
   if (TF_GetCode(status) != TF_OK) {
     std::cerr << "Error importing graph definition: " << TF_Message(status)
@@ -41,6 +41,6 @@ void AlexNetSample(std::string& path) {
 
   TF_DeleteGraph(graph);
   TF_DeleteSession(session, status);
-  TF_DeleteSessionOptions(sessionOptions);
+  TF_DeleteSessionOptions(session_options);
   TF_DeleteStatus(status);
 }
