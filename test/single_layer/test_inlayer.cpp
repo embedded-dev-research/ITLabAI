6d4bdfaefe78a7564f17e2f98a691084b470997c
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-03-08 16:42:05 +0300

    fix tidy

--- test/single_layer/test_inlayer.cpp
+++ test/single_layer/test_inlayer.cpp
@@ -4,13 +4,10 @@
 #include "gtest/gtest.h"
 #include "layers/InputLayer.hpp"
 
-std::string getPath() {
-  char* workspace = getenv("GITHUB_WORKSPACE");
-  return std::string(workspace) + "/build/test/image.jpg";
-}
-
 TEST(input, chech_basic) {
-  std::string input_file_name = getPath();
+  char* workspace = getenv("GITHUB_WORKSPACE");
+  std::string input_file_name =
+      std::string(workspace) + "/build/test/image.jpg";
   int n = 1;
   InputLayer<int> inlayer(n);
   std::vector<int> output = inlayer.run(input_file_name);
