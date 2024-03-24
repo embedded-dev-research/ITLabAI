9648b6b0c0d1969b746301bd1ead24e59fdda943
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-03-24 20:25:02 +0300

    tensot

--- include/layers/ConvLayer.hpp
+++ include/layers/ConvLayer.hpp
@@ -8,70 +8,55 @@
 template <typename ValueType>
 class ConvolutionalLayer : public Layer<ValueType> {
  public:
-  ConvolutionalLayer() = delete;
-  ConvolutionalLayer(const std::vector<ValueType>& kernel,
-                     const Shape& kernel_shape, size_t image_width,
-                     size_t image_height, int step);
+  ConvolutionalLayer() = default;
   std::vector<ValueType> run(
-      const std::vector<ValueType>& input) const override;
-
- private:
-  std::vector<ValueType> kernel_;
-  Shape kernel_shape_;
-  size_t image_width_;
-  size_t image_height_;
-  int step_;
+      const std::vector<ValueType>& input) const override {
+    return input;
+  };
+  void run(const Tensor& input, Tensor& output,
+           const std::vector<ValueType>& kernel, size_t step_);
 };
-
 template <typename ValueType>
-ConvolutionalLayer<ValueType>::ConvolutionalLayer(
-    const std::vector<ValueType>& kernel, const Shape& kernel_shape,
-    size_t image_width, size_t image_height, int step)
-    : Layer<ValueType>(Shape({image_height, image_width}),
-                       Shape({image_height - kernel_shape[0] - (step - 1),
-                              image_width - kernel_shape[1] - (step - 1)})),
-      kernel_(kernel),
-      kernel_shape_(kernel_shape),
-      image_width_(image_width),
-      image_height_(image_height),
-      step_(step) {}
-
-template <typename ValueType>
-std::vector<ValueType> ConvolutionalLayer<ValueType>::run(
-    const std::vector<ValueType>& input) const {
-  size_t input_size = input.size();
+void ConvolutionalLayer<ValueType>::run(const Tensor& input, Tensor& output,
+           const std::vector<ValueType>& kernel_, size_t step_) {
+  std::vector<ValueType> matrix = *input.as<ValueType>();
+  size_t input_size = matrix.size();
   size_t kernel_size = static_cast<int>(std::sqrt(kernel_.size()));
-  size_t output_height = this->outputShape_[0];
-  size_t output_width = this->outputShape_[1];
   int input_width = static_cast<int>(std::sqrt(input_size / 3));
-  if (input_size != image_width_ * image_height_ * 3) {
-    throw std::invalid_argument("Input size doesn't fit ConvolutionalLayer");
-  }
-  std::vector<ValueType> output;
-  for (int i = input_width + 1; i < static_cast<int>(input_size / 3);
-       i += step_) {
+  std::vector<ValueType> outputvec;
+  for (int i = input_width + static_cast<int>((kernel_size - 1)/2);
+       i < static_cast<int>(input_size / 3);
+       i += static_cast<int>(step_)) {
     for (int x = 0; x < 3; x++) {
       ValueType color = 0;
       int ckernel = 0;
       for (int coloms = -input_width; coloms < input_width + 1;
            coloms += input_width) {
         for (int str = -1; str < 2; str++) {
-          color += input[(i + coloms + str) * 3 + x] * kernel_[ckernel++];
+          color += matrix[(i + coloms + str) * 3 + x] * kernel_[ckernel++];
         }
       }
-      output.push_back(color);
+      outputvec.push_back(color);
     }
     if ((i + static_cast<int>((kernel_size - 1) / 2) + 1) % input_width == 0) {
       if (i + input_width + static_cast<int>((kernel_size - 1)) ==
           static_cast<int>(input_size / 3)) {
         i += input_width + static_cast<int>((kernel_size - 1)) + 1;
       } else {
-        i += input_width * (step_ - 1) + (3 - step_);
+        i += input_width * (static_cast<int>(step_) - 1) +
+             (3 - static_cast<int>(step_));
       }
     }
   }
-  if (output.size() != output_height * output_width * 3) {
-    throw std::invalid_argument("Output size doesn't fit ConvolutionalLayer");
-  }
-  return output;
+  Shape sh({1,
+            static_cast<size_t>(
+                ((input_width - 1 - static_cast<int>(kernel_size - 1)) /
+                 static_cast<int>(step_)) +
+                1),
+            static_cast<size_t>(
+                ((input_width - 1 - static_cast<int>(kernel_size - 1)) /
+                 static_cast<int>(step_)) +
+                1),
+            3});
+  output = make_tensor<float>(outputvec, sh);
 }
--- test/single_layer/test_convlayer.cpp
+++ test/single_layer/test_convlayer.cpp
@@ -7,21 +7,20 @@ TEST(ConvolutionalLayerTest, Step2) {
   for (float i = 0; i < 75; ++i) {
     image.push_back(1);
   }
-  size_t image_width = 5;
-  size_t image_height = 5;
+  Shape sh1({2, 2});
+  std::vector<int> vec = {1, 2, 3, 4};
+  Shape sh({1, 5, 5, 3});
+  Tensor input = make_tensor(image, sh);
+  Tensor output = make_tensor(vec, sh1);
   int step = 2;
   std::vector<float> kernel = {1, 0, 1, 0, 1, 0, 1, 0, 1};
-  std::vector<size_t> outsize;
-  outsize.push_back(2);
-  outsize.push_back(2);
-  Shape kernel_shape(outsize);
   std::vector<float> expected_output(12, 5);
-  ConvolutionalLayer<float> conv_layer(kernel, kernel_shape, image_width,
-                                       image_height, step);
-  std::vector<float> output = conv_layer.run(image);
-  ASSERT_EQ(output.size(), expected_output.size());
-  for (size_t i = 0; i < output.size(); ++i) {
-    ASSERT_FLOAT_EQ(output[i], expected_output[i]);
+  ConvolutionalLayer<float> conv_layer;
+  conv_layer.run(input, output, kernel, step);
+  std::vector<float> tmp = *output.as<float>();
+  ASSERT_EQ(tmp.size(), expected_output.size());
+  for (size_t i = 0; i < tmp.size(); ++i) {
+    ASSERT_FLOAT_EQ(tmp[i], expected_output[i]);
   }
 }
 TEST(ConvolutionalLayerTest, Step1) {
@@ -29,20 +28,19 @@ TEST(ConvolutionalLayerTest, Step1) {
   for (float i = 0; i < 75; ++i) {
     image.push_back(1);
   }
-  size_t image_width = 5;
-  size_t image_height = 5;
+  Shape sh1({2, 2});
+  std::vector<int> vec = {1, 2, 3, 4};
+  Shape sh({1, 5, 5, 3});
+  Tensor input = make_tensor(image, sh);
+  Tensor output = make_tensor(vec, sh1);
   int step = 1;
   std::vector<float> kernel = {1, 0, 1, 0, 1, 0, 1, 0, 1};
-  std::vector<size_t> outsize;
-  outsize.push_back(2);
-  outsize.push_back(2);
-  Shape kernel_shape(outsize);
   std::vector<float> expected_output(27, 5);
-  ConvolutionalLayer<float> conv_layer(kernel, kernel_shape, image_width,
-                                       image_height, step);
-  std::vector<float> output = conv_layer.run(image);
-  ASSERT_EQ(output.size(), expected_output.size());
-  for (size_t i = 0; i < output.size(); ++i) {
-    ASSERT_FLOAT_EQ(output[i], expected_output[i]);
+  ConvolutionalLayer<float> conv_layer;
+  conv_layer.run(input, output, kernel, step);
+  std::vector<float> tmp = *output.as<float>();
+  ASSERT_EQ(tmp.size(), expected_output.size());
+  for (size_t i = 0; i < tmp.size(); ++i) {
+    ASSERT_FLOAT_EQ(tmp[i], expected_output[i]);
   }
 }
