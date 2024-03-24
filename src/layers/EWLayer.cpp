846e91681372f1310b0625587b89d96a59e5b361
NeiroYT <119765880+NeiroYT@users.noreply.github.com>
2024-03-24 23:51:47 +0300

    Refactor all current layers using new Tensor class (#102)

--- src/layers/EWLayer.cpp
+++ src/layers/EWLayer.cpp
@@ -1,1 +1,21 @@
 #include "layers/EWLayer.hpp"
+
+void EWLayer::run(const Tensor &input, Tensor &output,
+                  const std::string &function) {
+  switch (input.get_type()) {
+    case Type::kInt: {
+      EWLayerImpl<int> used_impl(input.get_shape(), function);
+      output = make_tensor(used_impl.run(*input.as<int>()), input.get_shape());
+      break;
+    }
+    case Type::kFloat: {
+      EWLayerImpl<float> used_impl(input.get_shape(), function);
+      output =
+          make_tensor(used_impl.run(*input.as<float>()), input.get_shape());
+      break;
+    }
+    default: {
+      throw std::runtime_error("No such type");
+    }
+  }
+}
