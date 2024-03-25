91659a6c4053affca6b0903ecf305e46bfdeffa1
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-03-25 15:14:03 +0300

    fix

--- test/single_layer/test_convlayer.cpp
+++ test/single_layer/test_convlayer.cpp
@@ -2,7 +2,7 @@
 
 #include "layers/ConvLayer.hpp"
 
-TEST(ConvolutionalLayerTest, Step2) {
+TEST(ConvolutionalLayerTest, FStep2) {
   std::vector<float> image;
   image.reserve(75);
   for (int i = 0; i < 75; ++i) {
@@ -26,7 +26,7 @@ TEST(ConvolutionalLayerTest, Step2) {
     ASSERT_FLOAT_EQ(tmp[i], expected_output[i]);
   }
 }
-TEST(ConvolutionalLayerTest, Step1) {
+TEST(ConvolutionalLayerTest, FStep1) {
   std::vector<float> image;
   image.reserve(75);
   for (int i = 0; i < 75; ++i) {
@@ -48,5 +48,53 @@ TEST(ConvolutionalLayerTest, Step1) {
   ASSERT_EQ(tmp.size(), expected_output.size());
   for (size_t i = 0; i < tmp.size(); ++i) {
     ASSERT_FLOAT_EQ(tmp[i], expected_output[i]);
+  }
+}
+TEST(ConvolutionalLayerTest, IntStep2) {
+  std::vector<int> image;
+  image.reserve(75);
+  for (int i = 0; i < 75; ++i) {
+    image.push_back(1);
+  }
+  Shape sh({2, 2});
+  std::vector<int> vec = {1, 2, 3, 4};
+  Shape sh1({1, 5, 5, 3});
+  Tensor input = make_tensor(image, sh1);
+  Tensor output = make_tensor(vec, sh);
+  int step = 2;
+  std::vector<int> kernelvec = {1, 0, 1, 0, 1, 0, 1, 0, 1};
+  std::vector<int> expected_output(12, 5);
+  Shape sh2({3, 3});
+  Tensor kernel = make_tensor(kernelvec, sh2);
+  ConvolutionalLayer conv_layer;
+  conv_layer.run(input, output, kernel, step);
+  std::vector<int> tmp = *output.as<int>();
+  ASSERT_EQ(tmp.size(), expected_output.size());
+  for (size_t i = 0; i < tmp.size(); ++i) {
+    ASSERT_EQ(tmp[i], expected_output[i]);
+  }
+}
+TEST(ConvolutionalLayerTest, IntStep1) {
+  std::vector<int> image;
+  image.reserve(75);
+  for (int i = 0; i < 75; ++i) {
+    image.push_back(1);
+  }
+  Shape sh({2, 2});
+  std::vector<int> vec = {1, 2, 3, 4};
+  Shape sh1({1, 5, 5, 3});
+  Tensor input = make_tensor(image, sh1);
+  Tensor output = make_tensor(vec, sh);
+  int step = 1;
+  std::vector<int> kernelvec = {1, 0, 1, 0, 1, 0, 1, 0, 1};
+  std::vector<int> expected_output(27, 5);
+  Shape sh2({3, 3});
+  Tensor kernel = make_tensor(kernelvec, sh2);
+  ConvolutionalLayer conv_layer;
+  conv_layer.run(input, output, kernel, step);
+  std::vector<int> tmp = *output.as<int>();
+  ASSERT_EQ(tmp.size(), expected_output.size());
+  for (size_t i = 0; i < tmp.size(); ++i) {
+    ASSERT_EQ(tmp[i], expected_output[i]);
   }
 }
