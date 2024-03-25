5f1600c791911725d67ab217d7151d04d0c32468
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-03-25 15:16:44 +0300

    fix

--- include/layers/ConvLayer.hpp
+++ include/layers/ConvLayer.hpp
@@ -31,9 +31,8 @@ void ConvolutionalLayer::run(const Tensor& input, Tensor& output,
                coloms += input_width) {
             for (int str = -1; str < 2; str++) {
               size_t kercol = static_cast<size_t>(coloms / input_width + 1);
-              color +=
-                  matrix[(i + coloms + str) * 3 + x] *
-                  kernel_.get<int>({kercol, static_cast<size_t>(str + 1)});
+              color += matrix[(i + coloms + str) * 3 + x] *
+                       kernel_.get<int>({kercol, static_cast<size_t>(str + 1)});
             }
           }
           outputvec.push_back(color);
@@ -73,9 +72,8 @@ void ConvolutionalLayer::run(const Tensor& input, Tensor& output,
                coloms += input_width) {
             for (int str = -1; str < 2; str++) {
               size_t kercol = static_cast<size_t>(coloms / input_width + 1);
-              color +=
-                  matrix[(i + coloms + str) * 3 + x] *
-                  kernel_.get<float>({kercol, static_cast<size_t>(str + 1)});
+              color += matrix[(i + coloms + str) * 3 + x] *
+                       kernel_.get<float>({kercol, static_cast<size_t>(str + 1)});
             }
           }
           outputvec.push_back(color);
