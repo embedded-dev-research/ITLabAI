baaf805e52f188ca925c1927eddfa0b3da9e3579
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-03-26 19:49:11 +0300

    static

--- test/single_layer/test_inlayer.cpp
+++ test/single_layer/test_inlayer.cpp
@@ -11,8 +11,7 @@ TEST(input, chech_basic) {
   Shape sh1({2, 2});
   std::vector<int> vec = {1, 2, 3, 4};
   Tensor output = make_tensor(vec, sh1);
-  InputLayer inlayer;
-  inlayer.run(paths, output);
+  InputLayer::run(paths, output);
   std::vector<int> tmp = *output.as<int>();
   ASSERT_EQ(tmp.size(), 227 * 227 * 3);
 }
