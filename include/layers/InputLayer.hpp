b650565b78e76fa69ca4425f0fb32c01539a62fd
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-04-15 00:42:49 +0300

    fix

--- include/layers/InputLayer.hpp
+++ include/layers/InputLayer.hpp
@@ -6,23 +6,23 @@
 
 class InputLayer : public Layer {
  private:
-  bool layout_;
+  int layout_;
   int mean_;
   int std_;
 
  public:
   InputLayer() = default;
-  InputLayer(bool layout, int mean, int std) {
+  InputLayer(int layout, int mean, int std) {
     layout_ = layout;
     mean_ = mean;
     std_ = std;
   }  // layout = NCHW(0), NHWC(1)
-  void run(Tensor& input, Tensor& output) {
+  void run(Tensor& input, Tensor& output) const {
     switch (input.get_type()) {
       case Type::kInt: {
         std::vector<int> res = *input.as<int>();
-        for (size_t i = 0; i < res.size(); ++i) {
-          res[i] = static_cast<int>((res[i] - mean_) / std_);
+        for (int& re : res) {
+          re = static_cast<int>((re - mean_) / std_);
         }
         Shape sh(input.get_shape());
         output = make_tensor<int>(res, sh);
@@ -30,8 +30,8 @@ class InputLayer : public Layer {
       }
       case Type::kFloat: {
         std::vector<float> res = *input.as<float>();
-        for (size_t i = 0; i < res.size(); ++i) {
-          res[i] = static_cast<float>((res[i] - mean_) / std_);
+        for (float& re : res) {
+          re = static_cast<float>((re - mean_) / std_);
         }
         Shape sh(input.get_shape());
         output = make_tensor<float>(res, sh);
