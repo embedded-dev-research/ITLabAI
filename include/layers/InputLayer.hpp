5dee70ab0c933ed5ccc873c1061b2616da6b5316
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-04-14 23:27:44 +0300

    fixnormalisation

--- include/layers/InputLayer.hpp
+++ include/layers/InputLayer.hpp
@@ -1,41 +1,44 @@
 #pragma once
 #include <algorithm>
 #include <cmath>
-#include <cstdlib>
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <stdexcept>
-#include <string>
 
 #include "layers/Layer.hpp"
 
 class InputLayer : public Layer {
+ private:
+  bool layout_;
+  int mean_;
+  int std_;
  public:
   InputLayer() = default;
-  static void run(const std::vector<std::string>& path, Tensor& output) {
-    std::vector<int> res(path.size() * 227 * 227 * 3);
-    for (int num = 0; num < static_cast<int>(path.size()); num++) {
-      cv::Mat image = cv::imread(path[num]);
-      if (image.empty()) {
-        throw std::runtime_error("Failed to load image");
-      }
-      cv::Mat resized_image;
-      cv::resize(image, resized_image, cv::Size(227, 227));
-      std::vector<cv::Mat> channels;
-      cv::split(resized_image, channels);
-      int c = 0;
-      for (int i = 0; i < 227; ++i) {
-        for (int j = 0; j < 227; ++j) {
-          res[c] = static_cast<int>(channels[2].at<uchar>(i, j));
-          c++;
-          res[c] = static_cast<int>(channels[1].at<uchar>(i, j));
-          c++;
-          res[c] = static_cast<int>(channels[0].at<uchar>(i, j));
-          c++;
+  InputLayer(bool layout, int mean, int std) {
+    layout_ = layout;
+    mean_ = mean;
+    std_ = std;
+  }  // layout = NCHW(0), NHWC(1)
+  void run(Tensor& input, Tensor& output) {
+    switch (input.get_type()) {
+      case Type::kInt: {
+        std::vector<int> res = *input.as<int>();
+        for (size_t i = 0; i < res.size(); ++i) {
+          res[i] = static_cast<int>((res[i] - mean_) / std_);
         }
+        Shape sh(input.get_shape());
+        output = make_tensor<int>(res, sh);
+        break;
+      }
+      case Type::kFloat: {
+        std::vector<float> res = *input.as<float>();
+        for (size_t i = 0; i < res.size(); ++i) {
+          res[i] = static_cast<float>((res[i] - mean_) / std_);
+        }
+        Shape sh(input.get_shape());
+        output = make_tensor<float>(res, sh);
+        break;
+      }
+      default: {
+        throw std::runtime_error("No such type");
       }
     }
-    Shape sh({static_cast<size_t>(path.size()), 227, 227, 3});
-    output = make_tensor<int>(res, sh);
   }
 };
--- test/single_layer/test_inlayer.cpp
+++ test/single_layer/test_inlayer.cpp
@@ -5,13 +5,34 @@
 #include "layers/InputLayer.hpp"
 
 TEST(input, chech_basic) {
-  const std::string image_path = IMAGE_PATH;
-  std::vector<std::string> paths;
-  paths.push_back(image_path);
   Shape sh1({2, 2});
   std::vector<int> vec = {1, 2, 3, 4};
+  Tensor input = make_tensor(vec, sh1);
   Tensor output = make_tensor(vec, sh1);
-  InputLayer::run(paths, output);
+  InputLayer layer(0,1,2);
+  layer.run(input, output);
   std::vector<int> tmp = *output.as<int>();
-  ASSERT_EQ(tmp.size(), 227 * 227 * 3);
+  ASSERT_EQ(tmp.size(), 4);
+}
+TEST(input, run_int) {
+  Shape sh1({2, 2});
+  std::vector<int> vec = {3, 5, 7, 9};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(0, 1, 2);
+  layer.run(input, output);
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_float) {
+  Shape sh1({2, 2});
+  std::vector<float> vec = {3, 5, 7, 9};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(0, 1, 2);
+  layer.run(input, output);
+  std::vector<float> tmp = *output.as<float>();
+  std::vector<float> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
 }
