49035f547e4818df3c1b013eb608ccf2b0d6f38c
NeiroYT <119765880+NeiroYT@users.noreply.github.com>
2024-04-26 08:41:30 +0300

    Implement parametrized tests for pooling layer (#116)
    related to #83

--- test/single_layer/test_poolinglayer.cpp
+++ test/single_layer/test_poolinglayer.cpp
@@ -65,95 +65,61 @@ TEST(poolinglayer, equivalent_output_when_pool_size_1) {
   EXPECT_EQ(output_b, input);
 }
 
-TEST(poolinglayer, 1d_pooling_avg_test) {
-  Shape inpshape = {8};
-  Shape poolshape = {3};
+class PoolingTestsParameterized
+    : public ::testing::TestWithParam<
+          std::tuple<std::vector<double>, Shape, Shape, std::string,
+                     std::vector<double> > > {};
+// 1) input; 2) input_shape; 3) pooling_shape; 4) pooling_type;
+// 5) expected_output.
+
+TEST_P(PoolingTestsParameterized, pooling_works_correctly) {
+  auto data = GetParam();
+  std::vector<double> input = std::get<0>(data);
+  Shape inpshape = std::get<1>(data);
+  Shape poolshape = std::get<2>(data);
   PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "average");
-  std::vector<double> input({9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0});
+      PoolingLayerImpl<double>(inpshape, poolshape, std::get<3>(data));
   std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {8.0, 5.0, 2.5};
+  std::vector<double> true_output = std::get<4>(data);
   EXPECT_EQ(output, true_output);
 }
 
-TEST(poolinglayer, 1d_pooling_max_test) {
-  Shape inpshape = {8};
-  Shape poolshape = {3};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "max");
-  std::vector<double> input({9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {9.0, 6.0, 3.0};
-  EXPECT_EQ(output, true_output);
-}
+std::vector<double> basic_1d_data = {9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0};
+Shape basic_1d_shape = {8};
 
-TEST(poolinglayer, 1d_bigger_pooling_test) {
-  Shape inpshape = {8};
-  Shape poolshape = {9};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "average");
-  std::vector<double> input({9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {5.5};
-  EXPECT_EQ(output, true_output);
-}
+std::vector<double> basic_2d_1_data = {9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0,
+                                       2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
+Shape basic_2d_1_shape = {4, 4};
 
-TEST(poolinglayer, 2d_pooling_avg_test1) {
-  Shape inpshape = {4, 4};
-  Shape poolshape = {2, 2};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "average");
-  std::vector<double> input({9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 2.0, 3.0,
-                             4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {6.5, 4.5, 4.5, 6.5};
-  EXPECT_EQ(output, true_output);
-}
+std::vector<double> basic_2d_2_data = {9.0, 8.0, 7.0, 5.0, 4.0,
+                                       3.0, 2.0, 3.0, 4.0};
+Shape basic_2d_2_shape = {3, 3};
 
-TEST(poolinglayer, 2d_pooling_avg_test2) {
-  Shape inpshape = {3, 3};
-  Shape poolshape = {2, 2};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "average");
-  std::vector<double> input({9.0, 8.0, 7.0, 5.0, 4.0, 3.0, 2.0, 3.0, 4.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {6.5, 5.0, 2.5, 4.0};
-  EXPECT_EQ(output, true_output);
-}
-
-TEST(poolinglayer, 2d_pooling_max_test1) {
-  Shape inpshape = {4, 4};
-  Shape poolshape = {2, 2};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "max");
-  std::vector<double> input({9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 2.0, 3.0,
-                             4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {9.0, 7.0, 7.0, 9.0};
-  EXPECT_EQ(output, true_output);
-}
-
-TEST(poolinglayer, 2d_pooling_max_test2) {
-  Shape inpshape = {3, 3};
-  Shape poolshape = {2, 2};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "max");
-  std::vector<double> input({9.0, 8.0, 7.0, 5.0, 4.0, 3.0, 2.0, 3.0, 4.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {9.0, 7.0, 3.0, 4.0};
-  EXPECT_EQ(output, true_output);
-}
-
-TEST(poolinglayer, 2d_bigger_pooling_test) {
-  Shape inpshape = {3, 3};
-  Shape poolshape = {4, 4};
-  PoolingLayerImpl<double> a =
-      PoolingLayerImpl<double>(inpshape, poolshape, "average");
-  std::vector<double> input({9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0});
-  std::vector<double> output = a.run(input);
-  std::vector<double> true_output = {5.0};
-  EXPECT_EQ(output, true_output);
-}
+INSTANTIATE_TEST_SUITE_P(
+    pooling_tests, PoolingTestsParameterized,
+    ::testing::Values(
+        std::make_tuple(basic_1d_data, basic_1d_shape, Shape({3}),
+                        std::string("average"),
+                        std::vector<double>({8.0, 5.0, 2.5})),
+        std::make_tuple(basic_1d_data, basic_1d_shape, Shape({3}),
+                        std::string("max"),
+                        std::vector<double>({9.0, 6.0, 3.0})),
+        std::make_tuple(basic_1d_data, basic_1d_shape, Shape({9}),
+                        std::string("average"), std::vector<double>({5.5})),
+        std::make_tuple(basic_2d_1_data, basic_2d_1_shape, Shape({2, 2}),
+                        std::string("average"),
+                        std::vector<double>({6.5, 4.5, 4.5, 6.5})),
+        std::make_tuple(basic_2d_1_data, basic_2d_1_shape, Shape({2, 2}),
+                        std::string("max"),
+                        std::vector<double>({9.0, 7.0, 7.0, 9.0})),
+        std::make_tuple(basic_2d_2_data, basic_2d_2_shape, Shape({2, 2}),
+                        std::string("average"),
+                        std::vector<double>({6.5, 5.0, 2.5, 4.0})),
+        std::make_tuple(basic_2d_2_data, basic_2d_2_shape, Shape({2, 2}),
+                        std::string("max"),
+                        std::vector<double>({9.0, 7.0, 3.0, 4.0})),
+        std::make_tuple(basic_2d_2_data, basic_2d_2_shape, Shape({4, 4}),
+                        std::string("average"), std::vector<double>({5.0}))));
 
 TEST(poolinglayer, new_pooling_layer_can_run_float_avg) {
   Shape inpshape = {4, 4};
