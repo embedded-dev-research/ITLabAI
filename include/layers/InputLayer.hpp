bf8f2dc912a9f4f2757510f39bf106daa933ac91
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-04-27 12:47:03 +0300

    fix

--- include/layers/InputLayer.hpp
+++ include/layers/InputLayer.hpp
@@ -4,15 +4,22 @@
 
 #include "layers/Layer.hpp"
 
+enum LayInOut {
+  NCHW,  // 0
+  NHWC   // 1
+};
+
 class InputLayer : public Layer {
  private:
-  int layout_;
+  LayInOut layin_;
+  LayInOut layout_;
   int mean_;
   int std_;
 
  public:
   InputLayer() = default;
-  InputLayer(int layout, int mean, int std) {
+  InputLayer(LayInOut layin, LayInOut layout, int mean, int std) {
+    layin_ = layin;
     layout_ = layout;
     mean_ = mean;
     std_ = std;
@@ -20,21 +27,117 @@ class InputLayer : public Layer {
   void run(Tensor& input, Tensor& output) const {
     switch (input.get_type()) {
       case Type::kInt: {
-        std::vector<int> res = *input.as<int>();
-        for (int& re : res) {
+        std::vector<int> in = *input.as<int>();
+        for (int& re : in) {
           re = static_cast<int>((re - mean_) / std_);
         }
         Shape sh(input.get_shape());
-        output = make_tensor<int>(res, sh);
+        if (layin_ == NCHW && layout_ == NHWC) {
+          int N = static_cast<int>(sh[0]);
+          int C = static_cast<int>(sh[1]);
+          int H = static_cast<int>(sh[2]);
+          int W = static_cast<int>(sh[3]);
+          std::vector<int> res(N * H * W * C);
+          for (int n = 0; n < N; ++n) {
+            for (int c = 0; c < C; ++c) {
+              for (int h = 0; h < H; ++h) {
+                for (int w = 0; w < W; ++w) {
+                  int nchw_index = n * C * H * W + c * H * W + h * W + w;
+                  int nhwc_index = n * H * W * C + h * W * C + w * C + c;
+                  res[nhwc_index] = in[nchw_index];
+                }
+              }
+            }
+          }
+          Shape sh1({static_cast<unsigned long long>(N),
+                     static_cast<unsigned long long>(H),
+                     static_cast<unsigned long long>(W),
+                     static_cast<unsigned long long>(C)});
+          output = make_tensor<int>(res, sh1);
+          break;
+        }
+        if (layin_ == NHWC && layout_ == NCHW) {
+          int N = static_cast<int> (sh[0]);
+          int C = static_cast<int> (sh[3]);
+          int H = static_cast<int> (sh[1]);
+          int W = static_cast<int> (sh[2]);
+          std::vector<int> res(N * C * H * W);
+          for (int n = 0; n < N; ++n) {
+            for (int c = 0; c < C; ++c) {
+              for (int h = 0; h < H; ++h) {
+                for (int w = 0; w < W; ++w) {
+                  int nhwc_index = n * H * W * C + h * W * C + w * C + c;
+                  int nchw_index = n * C * H * W + c * H * W + h * W + w;
+                  res[nchw_index] = in[nhwc_index];
+                }
+              }
+            }
+          }
+          Shape sh1({static_cast<unsigned long long>(N),
+                     static_cast<unsigned long long>(C),
+                     static_cast<unsigned long long>(H),
+                     static_cast<unsigned long long>(W)});
+          output = make_tensor<int>(res, sh1);
+          break;
+        }
+        output = make_tensor<int>(in, sh);
         break;
       }
       case Type::kFloat: {
-        std::vector<float> res = *input.as<float>();
-        for (float& re : res) {
+        std::vector<float> in = *input.as<float>();
+        for (float& re : in) {
           re = static_cast<float>((re - mean_) / std_);
         }
         Shape sh(input.get_shape());
-        output = make_tensor<float>(res, sh);
+        if (layin_ == NCHW && layout_ == NHWC) {
+          int N = static_cast<int>(sh[0]);
+          int C = static_cast<int>(sh[1]);
+          int H = static_cast<int>(sh[2]);
+          int W = static_cast<int>(sh[3]);
+          std::vector<float> res(N * H * W * C);
+          for (int n = 0; n < N; ++n) {
+            for (int c = 0; c < C; ++c) {
+              for (int h = 0; h < H; ++h) {
+                for (int w = 0; w < W; ++w) {
+                  int nchw_index = n * C * H * W + c * H * W + h * W + w;
+                  int nhwc_index = n * H * W * C + h * W * C + w * C + c;
+                  res[nhwc_index] = in[nchw_index];
+                }
+              }
+            }
+          }
+          Shape sh1({static_cast<unsigned long long>(N),
+                     static_cast<unsigned long long>(H),
+                     static_cast<unsigned long long>(W),
+                     static_cast<unsigned long long>(C)});
+          output = make_tensor<float>(res, sh1);
+          break;
+        }
+        if (layin_ == NHWC && layout_ == NCHW) {
+          int N = static_cast<int>(sh[0]);
+          int C = static_cast<int>(sh[3]);
+          int H = static_cast<int>(sh[1]);
+          int W = static_cast<int>(sh[2]);
+          std::vector<float> res(N * C * H * W);
+          for (int n = 0; n < N; ++n) {
+            for (int c = 0; c < C; ++c) {
+              for (int h = 0; h < H; ++h) {
+                for (int w = 0; w < W; ++w) {
+                  int nhwc_index = n * H * W * C + h * W * C + w * C + c;
+                  int nchw_index = n * C * H * W + c * H * W + h * W + w;
+                  res[nchw_index] = in[nhwc_index];
+                }
+              }
+            }
+          }
+          Shape sh1({static_cast<unsigned long long>(N),
+                     static_cast<unsigned long long>(C),
+                     static_cast<unsigned long long>(H),
+                     static_cast<unsigned long long>(W)});
+          output = make_tensor<float>(res, sh1);
+          break;
+        }
+        output = make_tensor<float>(in, sh);
         break;
       }
       default: {
--- test/single_layer/test_inlayer.cpp
+++ test/single_layer/test_inlayer.cpp
@@ -5,34 +5,89 @@
 #include "layers/InputLayer.hpp"
 
 TEST(input, chech_basic) {
-  Shape sh1({2, 2});
+  Shape sh1({1, 2, 2, 1});
   std::vector<int> vec = {1, 2, 3, 4};
   Tensor input = make_tensor(vec, sh1);
   Tensor output = make_tensor(vec, sh1);
-  InputLayer layer(0, 1, 2);
+  InputLayer layer(NHWC, NHWC, 1, 2);
   layer.run(input, output);
   std::vector<int> tmp = *output.as<int>();
   ASSERT_EQ(tmp.size(), 4);
 }
 TEST(input, run_int) {
-  Shape sh1({2, 2});
+  Shape sh1({1, 2, 2, 1});
   std::vector<int> vec = {3, 5, 7, 9};
   Tensor input = make_tensor(vec, sh1);
   Tensor output = make_tensor(vec, sh1);
-  InputLayer layer(0, 1, 2);
+  InputLayer layer(NHWC, NHWC, 1, 2);
   layer.run(input, output);
   std::vector<int> tmp = *output.as<int>();
   std::vector<int> res = {1, 2, 3, 4};
   ASSERT_EQ(tmp, res);
 }
 TEST(input, run_float) {
-  Shape sh1({2, 2});
+  Shape sh1({1, 2, 2, 1});
   std::vector<float> vec = {3, 5, 7, 9};
   Tensor input = make_tensor(vec, sh1);
   Tensor output = make_tensor(vec, sh1);
-  InputLayer layer(0, 1, 2);
+  InputLayer layer(NHWC, NHWC, 1, 2);
   layer.run(input, output);
   std::vector<float> tmp = *output.as<float>();
   std::vector<float> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_int_NCHW_NHWC) {
+  Shape sh1({1, 1, 2, 2});
+  std::vector<int> vec = {3, 5, 7, 9};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(NCHW, NHWC, 1, 2);
+  layer.run(input, output);
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_float_NCHW_NHWC) {
+  Shape sh1({1, 1, 2, 2});
+  std::vector<int> vec = {3, 5, 7, 9};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(NCHW, NHWC, 1, 2);
+  layer.run(input, output);
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_int_NHWC_NCHW) {
+  Shape sh1({1, 2, 2, 1});
+  std::vector<int> vec = {3, 5, 7, 9};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(NHWC, NCHW, 1, 2);
+  layer.run(input, output);
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_float_NHWC_NCHW) {
+  Shape sh1({1, 2, 2, 1});
+  std::vector<int> vec = {3, 5, 7, 9};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(NHWC, NCHW, 1, 2);
+  layer.run(input, output);
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {1, 2, 3, 4};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_int_NHWC_NCHW_C3) {
+  Shape sh1({1, 2, 2, 3});
+  std::vector<int> vec = {3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(NHWC, NCHW, 1, 2);
+  layer.run(input, output);
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12};
   ASSERT_EQ(tmp, res);
 }
