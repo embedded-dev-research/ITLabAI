8b499af5742e2c6aa971150dd163d83cbe5b7e90
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-04-27 17:02:57 +0300

    fix

--- test/single_layer/test_inlayer.cpp
+++ test/single_layer/test_inlayer.cpp
@@ -51,13 +51,13 @@ TEST(input, run_int_NCHW_NHWC) {
 }
 TEST(input, run_float_NCHW_NHWC) {
   Shape sh1({1, 1, 2, 2});
-  std::vector<int> vec = {3, 5, 7, 9};
+  std::vector<float> vec = {3, 5, 7, 9};
   Tensor input = make_tensor(vec, sh1);
   Tensor output = make_tensor(vec, sh1);
   InputLayer layer(NCHW, NHWC, 1, 2);
   layer.run(input, output);
-  std::vector<int> tmp = *output.as<int>();
-  std::vector<int> res = {1, 2, 3, 4};
+  std::vector<float> tmp = *output.as<float>();
+  std::vector<float> res = {1, 2, 3, 4};
   ASSERT_EQ(tmp, res);
 }
 TEST(input, run_int_NHWC_NCHW) {
@@ -73,13 +73,13 @@ TEST(input, run_int_NHWC_NCHW) {
 }
 TEST(input, run_float_NHWC_NCHW) {
   Shape sh1({1, 2, 2, 1});
-  std::vector<int> vec = {3, 5, 7, 9};
+  std::vector<float> vec = {3, 5, 7, 9};
   Tensor input = make_tensor(vec, sh1);
   Tensor output = make_tensor(vec, sh1);
   InputLayer layer(NHWC, NCHW, 1, 2);
   layer.run(input, output);
-  std::vector<int> tmp = *output.as<int>();
-  std::vector<int> res = {1, 2, 3, 4};
+  std::vector<float> tmp = *output.as<float>();
+  std::vector<float> res = {1, 2, 3, 4};
   ASSERT_EQ(tmp, res);
 }
 TEST(input, run_int_NHWC_NCHW_C3) {
@@ -91,5 +91,16 @@ TEST(input, run_int_NHWC_NCHW_C3) {
   layer.run(input, output);
   std::vector<int> tmp = *output.as<int>();
   std::vector<int> res = {1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12};
+  ASSERT_EQ(tmp, res);
+}
+TEST(input, run_float_NHWC_NCHW_C3) {
+  Shape sh1({1, 2, 2, 3});
+  std::vector<float> vec = {3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25};
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer layer(NHWC, NCHW, 1, 2);
+  layer.run(input, output);
+  std::vector<float> tmp = *output.as<float>();
+  std::vector<float> res = {1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12};
   ASSERT_EQ(tmp, res);
 }
