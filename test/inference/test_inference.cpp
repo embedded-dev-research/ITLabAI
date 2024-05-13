f5b5fc71f8956355268ad32b8c1b65917a3186f1
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-05-13 17:05:56 +0300

    inf

--- test/inference/test_inference.cpp
+++ test/inference/test_inference.cpp
@@ -6,6 +6,8 @@
 #include "layers/EWLayer.hpp"
 #include "layers/InputLayer.hpp"
 #include "layers/OutputLayer.hpp"
+#include "layers/PoolingLayer.hpp"
+#include "layers/FCLayer.hpp"
 
 using namespace itlab_2023;
 
@@ -31,6 +33,38 @@ TEST(bfs, check_result_vec) {
   graph.makeConnection(a1, a3);
   graph.makeConnection(a2, a4);
   graph.setOutput(a4, output);
+  graph.inference();
+  std::vector<int> tmp = *output.as<int>();
+  std::vector<int> res = {81, 81, 81};
+  ASSERT_EQ(tmp, res);
+}
+TEST(bfs, check_e2e) {
+  Graph graph(6);
+  Shape sh1({1, 1, 25, 3});
+  std::vector<int> vec;
+  vec.reserve(75);
+  for (int i = 0; i < 75; ++i) {
+    vec.push_back(3);
+  }
+  Tensor input = make_tensor(vec, sh1);
+  Tensor output = make_tensor(vec, sh1);
+  InputLayer a1(kNhwc, kNhwc, 1, 2);
+  std::vector<int> kernelvec = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+  Shape sh2({3, 3});
+  Tensor kernel = make_tensor(kernelvec, sh2);
+  ConvolutionalLayer a2(1, 1, 0, kernel);
+  Shape poolshape = {2, 2, 2};
+  PoolingLayer a3(poolshape, "average");
+  EWLayer a4;
+  FCLayer a5;
+  OutputLayer a6;
+  graph.setInput(a1, input);
+  graph.makeConnection(a1, a2);
+  graph.makeConnection(a2, a3);
+  graph.makeConnection(a3, a4);
+  graph.makeConnection(a4, a5);
+  graph.makeConnection(a5, a6);
+  graph.setOutput(a2, output);
   graph.inference();
   std::vector<int> tmp = *output.as<int>();
   std::vector<int> res = {81, 81, 81};
