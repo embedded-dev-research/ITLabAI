c7b089d1f2c45c3f45e2b45c3d1fc4d5e0700306
Semyon1104 <sema_titov_04@mail.ru>
2024-07-23 19:31:56 +0300

    add tests

--- test/single_layer/test_convlayer.cpp
+++ test/single_layer/test_convlayer.cpp
@@ -129,3 +129,40 @@ TEST(ConvolutionalLayerTest, FloatWithBias) {
     ASSERT_FLOAT_EQ(tmp[i], expected_output[i]);
   }
 }
+
+TEST(ConvolutionalLayerTest, InvalidInputShapeDims) {
+  std::vector<float> image(15, 1.0f);
+  Shape invalid_shape({1, 3, 5});
+  Tensor input = make_tensor(image, invalid_shape);
+
+  std::vector<float> kernelvec = {1, 0, 1, 0, 1, 0, 1, 0, 1};
+  Shape kernel_shape({3, 3});
+  Tensor kernel = make_tensor(kernelvec, kernel_shape);
+
+  Shape output_shape({1, 3, 3, 3});
+  std::vector<float> output_vec(27, 0.0f);
+  Tensor output = make_tensor(output_vec, output_shape);
+
+  ConvolutionalLayer layer(1, 0, 0, kernel);
+
+  EXPECT_THROW(layer.run(input, output), std::out_of_range);
+}
+
+TEST(ConvImplTest, RunReturnsInput) {
+  std::vector<float> input = {1.0, 2.0, 3.0, 4.0};
+  ConvImpl<float> conv(1, 0, 1, 2, 2, 1, 4, {0.0});
+
+  std::vector<float> output = conv.run(input);
+
+  ASSERT_EQ(output, input);
+}
+
+
+TEST(ConvImplTest, ThrowsOnZeroInputWidth) {
+  std::vector<float> input = {1.0, 2.0, 3.0, 4.0};
+  ConvImpl<float> conv(1, 0, 1, 0, 2, 1, 4, {0.0});
+
+  ASSERT_THROW({ std::vector<float> output = conv.run(input); },
+               std::out_of_range);
+}
+
