876dfdab2f8732b99d0e32c817df3d7fc565b039
NeiroYT <119765880+NeiroYT@users.noreply.github.com>
2024-10-25 17:58:39 +0300

    Update test_flattenlayer.cpp

--- test/single_layer/test_flattenlayer.cpp
+++ test/single_layer/test_flattenlayer.cpp
@@ -11,7 +11,8 @@ TEST(flattenlayer, new_flattenlayer_can_flatten_int) {
   Tensor input = make_tensor<int>({1, -1, 2, -2}, sh);
   Tensor output;
   layer.run(input, output);
-  EXPECT_EQ(output.get_shape(), Shape({4}));
+  EXPECT_EQ(output.get_shape().dims(), 1);
+  EXPECT_EQ(output.get_shape()[0], 4);
 }
 
 TEST(flattenlayer, new_flattenlayer_can_flatten_float) {
@@ -20,7 +21,8 @@ TEST(flattenlayer, new_flattenlayer_can_flatten_float) {
   Tensor input = make_tensor<float>({1.0F, -1.0F, 2.0F, -2.0F}, sh);
   Tensor output;
   layer.run(input, output);
-  EXPECT_EQ(output.get_shape(), Shape({4}));
+  EXPECT_EQ(output.get_shape().dims(), 1);
+  EXPECT_EQ(output.get_shape()[0], 4);
 }
 
 TEST(flattenlayer, get_layer_name) {
