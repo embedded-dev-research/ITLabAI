feb5271f810e4950de17c8604fb2db7c7c79020d
NeiroYT <119765880+NeiroYT@users.noreply.github.com>
2024-10-25 17:45:20 +0300

    2

---
+++ src/layers/FlattenLayer.cpp
@@ -0,0 +1,23 @@
+#include "layers/FlattenLayer.hpp"
+
+namespace itlab_2023 {
+
+void FlattenLayer::run(const Tensor &input, Tensor &output) {
+  switch (input.get_type()) {
+    case Type::kInt: {
+      output =
+	      make_tensor(*input.as<int>(), Shape({input.get_shape().count()}));
+      break;
+    }
+    case Type::kFloat: {
+      output =
+          make_tensor(*input.as<float>(), Shape({input.get_shape().count()}));
+      break;
+    }
+    default: {
+      throw std::runtime_error("No such type");
+    }
+  }
+}
+
+}  // namespace itlab_2023
