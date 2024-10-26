b004449519bea1f355c256a50909345e561820cf
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-10-26 19:09:02 +0300

    dropout

--- src/layers/DropOutLayer.cpp
+++ src/layers/DropOutLayer.cpp
@@ -7,14 +7,14 @@
 namespace itlab_2023 {
 
 void DropOutLayer::run(const Tensor &input, Tensor &output) {
+  const double lower_bound = 0;
+  const double upper_bound = 100;
+  std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
+  std::random_device rand_dev;
+  std::mt19937 rand_engine(rand_dev());
   switch (input.get_type()) {
     case Type::kInt: {
       std::vector<int> vec = *input.as<int>();
-      const double lower_bound = 0;
-      const double upper_bound = 100;
-      std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
-      std::random_device rand_dev;
-      std::mt19937 rand_engine(rand_dev());
       for (size_t i = 0; i < vec.size(); i++) {
         if (unif(rand_engine) < static_cast<float>(drop_rate_) * 100)
           vec[i] = 0;
@@ -24,11 +24,6 @@ void DropOutLayer::run(const Tensor &input, Tensor &output) {
     }
     case Type::kFloat: {
       std::vector<float> vec = *input.as<float>();
-      const double lower_bound = 0;
-      const double upper_bound = 100;
-      std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
-      std::random_device rand_dev;
-      std::mt19937 rand_engine(rand_dev());
       for (size_t i = 0; i < vec.size(); i++) {
         if (unif(rand_engine) < static_cast<float>(drop_rate_) * 100)
           vec[i] = 0;
