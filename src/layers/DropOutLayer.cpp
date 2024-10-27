d76fc5fbf287026da8dedd6a2858def1f5f21d3e
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-10-27 14:29:09 +0300

    fix

--- src/layers/DropOutLayer.cpp
+++ src/layers/DropOutLayer.cpp
@@ -15,18 +15,18 @@ void DropOutLayer::run(const Tensor &input, Tensor &output) {
   switch (input.get_type()) {
     case Type::kInt: {
       std::vector<int> vec = *input.as<int>();
-      for (size_t i = 0; i < vec.size(); ++i) {
+      for (int &i : vec) {
         if (unif(rand_engine) < static_cast<float>(drop_rate_) * 100)
-          vec[i] = 0;
+          i = 0;
       }
       output = make_tensor(vec, input.get_shape());
       break;
     }
     case Type::kFloat: {
       std::vector<float> vec = *input.as<float>();
-      for (size_t i = 0; i < vec.size(); ++i) {
+      for (float &i : vec) {
         if (unif(rand_engine) < static_cast<float>(drop_rate_) * 100)
-          vec[i] = 0;
+          i = 0;
       }
       output = make_tensor(vec, input.get_shape());
       break;
