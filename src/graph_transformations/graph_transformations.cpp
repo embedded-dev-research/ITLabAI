3593b67f0afb328cb5f525d589b86af0b1038b3f
NeiroYT <neiro15000000let@gmail.com>
2025-09-18 15:19:47 +0300

    Changed logic

--- src/graph_transformations/graph_transformations.cpp
+++ src/graph_transformations/graph_transformations.cpp
@@ -1,4 +1,4 @@
-#include "graph/graph_transformations.hpp"
+#include "graph_transformations/graph_transformations.hpp"
 
 namespace it_lab_ai {
 
@@ -21,17 +21,27 @@ bool layer_conditions(const Layer& layer, const Layer& layer_sub) {
   return layer.getName() == layer_sub.getName();
 }
 
+//void erase_inequality_for_first(
+//    std::vector<std::pair<int, LayerType>>& vec1,
+//    const std::vector<std::pair<int, LayerType>>& vec2) {
+//  for (int i = 0; i < std::min(vec1.size(), vec2.size()); i++) {
+//    if (vec1[i].second != vec2[i].second) {
+//      vec1.erase(vec1.begin() + i);
+//    }
+//  }
+//  if (vec1.size() > vec2.size()) {
+//    vec1.resize(vec2.size());
+//  }
+//}
+
 bool check_child(const Graph& graph, const Graph& subgraph, int i, int iter) {
-  int amount_connected1 =
-      (i < graph.getLayersCount() - 1)
-          ? (graph.getVertexValue(i + 1) - graph.getVertexValue(i))
-          : 0;
+  int amount_connected1 = graph.getVertexValue(i + 1) - graph.getVertexValue(i);
   int amount_connected2 =
-      (iter < subgraph.getLayersCount() - 1)
-          ? (subgraph.getVertexValue(iter + 1) - subgraph.getVertexValue(iter))
-          : 0;
-  if ((amount_connected2 != 0 && amount_connected1 != amount_connected2) ||
-      !layer_conditions(*graph.getLayerFromID(i),
+      subgraph.getVertexValue(iter + 1) - subgraph.getVertexValue(iter);
+  if (amount_connected1 != amount_connected2 && amount_connected2 != 0) {
+    return false;
+  }
+  if (!layer_conditions(*graph.getLayerFromID(i),
                         *subgraph.getLayerFromID(iter))) {
     return false;
   }
@@ -53,13 +63,20 @@ bool check_child(const Graph& graph, const Graph& subgraph, int i, int iter) {
     }
     std::sort(orderA.begin(), orderA.end(),
               [&](id_name a1, id_name a2) { return a1.second < a2.second; });
+    std::sort(orderB.begin(), orderB.end(),
+              [&](id_name a1, id_name a2) { return a1.second < a2.second; });
     // ^ interested in LayerType order to prevent any shuffling for childs
+    //if (first_inequally) {
+    //  erase_inequality_for_first(orderA, orderB);
+    //}
     for (int j = 0; j < amount_connected1; j++) {
-      if (graph.getInputsSize(j) != subgraph.getInputsSize(iter)) {
+      int id1 = graph.getEdgeValue(graph.getVertexValue(i) + j);
+      int id2 = subgraph.getEdgeValue(subgraph.getVertexValue(iter) + j);
+      if (graph.getInputsSize(id1) != subgraph.getInputsSize(id2)) {
         return false;
       }
       bool temp =
-          check_child(graph, subgraph, orderA[j].first, orderB[j].second);
+          check_child(graph, subgraph, orderA[j].first, orderB[j].first);
       if (!temp) {
         return false;
       }
