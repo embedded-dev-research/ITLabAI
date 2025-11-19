69a131370c50eb2a0ec5ca713d0c6c9243cd0bc3
Semyon1104 <sema_titov_04@mail.ru>
2025-11-19 19:10:20 +0300

    add tests for graph

--- test/graph/test_graph.cpp
+++ test/graph/test_graph.cpp
@@ -5,9 +5,11 @@
 #include "graph/graph.hpp"
 #include "graph_transformations/graph_transformations.hpp"
 #include "gtest/gtest.h"
+#include "layers/ConcatLayer.hpp"
 #include "layers/EWLayer.hpp"
 #include "layers/FCLayer.hpp"
 #include "layers/InputLayer.hpp"
+#include "layers/SplitLayer.hpp"
 #include "perf/benchmarking.hpp"
 
 using namespace it_lab_ai;
@@ -144,6 +146,117 @@ TEST(graph, check_connection_when_not_connection2) {
   graph.setOutput(fcLayer4_ptr, output);
 
   ASSERT_EQ(graph.areLayerNext(fcLayer2_ptr, fcLayer4_ptr), 0);
+}
+
+TEST(graph, set_split_distribution) {
+  Graph graph;
+  std::vector<std::vector<std::pair<int, int>>> split_dist = {
+      {{1, 0}, {2, 1}}, {{3, 0}, {4, 0}, {5, 1}}};
+  graph.setSplitDistribution(split_dist);
+  SUCCEED();
+}
+
+TEST(graph, set_input_null_layer) {
+  Graph graph;
+  Tensor input = make_tensor<float>({1.0F, 2.0F}, {2});
+  EXPECT_THROW(graph.setInput(nullptr, input), std::invalid_argument);
+}
+
+TEST(graph, add_owned_layer_null_check) {
+  Graph graph;
+  std::unique_ptr<Layer> null_layer = nullptr;
+  graph.addOwnedLayer(std::move(null_layer));
+  SUCCEED();
+}
+
+TEST(graph, make_connection_null_layers) {
+  Graph graph;
+  Tensor input = make_tensor<float>({1.0F, 2.0F}, {2});
+  auto valid_layer = std::make_unique<InputLayer>();
+  Layer* valid_ptr = valid_layer.get();
+  graph.addOwnedLayer(std::move(valid_layer));
+
+  EXPECT_THROW(graph.makeConnection(nullptr, valid_ptr), std::invalid_argument);
+  EXPECT_THROW(graph.makeConnection(valid_ptr, nullptr), std::invalid_argument);
+  EXPECT_THROW(graph.makeConnection(nullptr, nullptr), std::invalid_argument);
+}
+
+TEST(graph, make_connection_same_layer) {
+  Graph graph;
+  Tensor input = make_tensor<float>({1.0F, 2.0F}, {2});
+  auto layer = std::make_unique<InputLayer>();
+  Layer* layer_ptr = layer.get();
+  graph.addOwnedLayer(std::move(layer));
+  graph.setInput(layer_ptr, input);
+
+  EXPECT_THROW(graph.makeConnection(layer_ptr, layer_ptr), std::out_of_range);
+}
+
+TEST(graph, set_output_null_layer) {
+  Graph graph;
+  Tensor output;
+  EXPECT_THROW(graph.setOutput(nullptr, output), std::invalid_argument);
+}
+
+TEST(graph, get_vertex_value_invalid_id) {
+  Graph graph;
+  EXPECT_THROW(graph.getVertexValue(1000), std::invalid_argument);
+}
+
+TEST(graph, get_edge_value_invalid_pos) {
+  Graph graph;
+  EXPECT_THROW(graph.getEdgeValue(1000), std::invalid_argument);
+}
+
+TEST(graph, get_inputs_size_invalid_id) {
+  Graph graph;
+  EXPECT_THROW(graph.getInputsSize(1000), std::invalid_argument);
+}
+
+TEST(graph, get_layer_from_id_invalid_id) {
+  Graph graph;
+  EXPECT_THROW(graph.getLayerFromID(1000), std::invalid_argument);
+}
+
+TEST(graph, complex_graph_with_split_distribution) {
+  std::vector<std::vector<std::pair<int, int>>> split_dist = {{{2, 0}, {3, 1}}};
+
+  Graph graph(10, split_dist);
+  Tensor input = make_tensor<float>({1.0F, 2.0F, 3.0F, 4.0F}, {2, 2});
+  Tensor output;
+
+  auto input_layer = std::make_unique<InputLayer>();
+  auto split_layer = std::make_unique<SplitLayer>(1, 2);
+  auto ew_layer1 = std::make_unique<EWLayer>("relu");
+  auto ew_layer2 = std::make_unique<EWLayer>("sigmoid");
+  auto concat_layer = std::make_unique<ConcatLayer>(0);
+
+  Layer* input_ptr = input_layer.get();
+  Layer* split_ptr = split_layer.get();
+  Layer* ew1_ptr = ew_layer1.get();
+  Layer* ew2_ptr = ew_layer2.get();
+  Layer* concat_ptr = concat_layer.get();
+  graph.setSplitDistribution(split_dist);
+
+  graph.addOwnedLayer(std::move(input_layer));
+  graph.addOwnedLayer(std::move(split_layer));
+  graph.addOwnedLayer(std::move(ew_layer1));
+  graph.addOwnedLayer(std::move(ew_layer2));
+  graph.addOwnedLayer(std::move(concat_layer));
+
+  graph.setInput(input_ptr, input);
+  graph.makeConnection(input_ptr, split_ptr);
+  graph.makeConnection(split_ptr, ew1_ptr);
+  graph.makeConnection(split_ptr, ew2_ptr);
+  graph.makeConnection(ew1_ptr, concat_ptr);
+  graph.makeConnection(ew2_ptr, concat_ptr);
+  graph.setOutput(concat_ptr, output);
+
+  ASSERT_TRUE(graph.areLayerNext(input_ptr, split_ptr));
+  ASSERT_TRUE(graph.areLayerNext(split_ptr, ew1_ptr));
+  ASSERT_TRUE(graph.areLayerNext(split_ptr, ew2_ptr));
+  ASSERT_TRUE(graph.areLayerNext(ew1_ptr, concat_ptr));
+  ASSERT_TRUE(graph.areLayerNext(ew2_ptr, concat_ptr));
 }
 
 TEST(graph, vertex_out_of_range) {
