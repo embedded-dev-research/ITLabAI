bb56e13bec5094cb40626635ea0799a6135172a9
AndreySorokin7 <129724280+AndreySorokin7@users.noreply.github.com>
2025-12-20 16:56:46 +0300

    Update backends.hpp

--- include/parallel/backends.hpp
+++ include/parallel/backends.hpp
@@ -2,8 +2,6 @@
 #include <oneapi/tbb/blocked_range.h>
 #include <oneapi/tbb/info.h>
 #include <oneapi/tbb/parallel_for.h>
-
-// NOLINTNEXTLINE
 #include <Kokkos_Core.hpp>
 
 #include <cstddef>
@@ -156,4 +154,5 @@ inline void impl_kokkos(std::size_t count,
 
 }  // namespace parallel
 }  // namespace it_lab_ai
+
 
