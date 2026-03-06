6e0a91b2b11ad934fad678547015e68d46920290
NeiroYT <neiro15000000let@gmail.com>
2026-03-06 18:27:06 +0300

    Test fix

--- test/single_layer/test_convlayer.cpp
+++ test/single_layer/test_convlayer.cpp
@@ -18,9 +18,13 @@ TEST(ConvolutionalLayerTest, Getters) {
   Tensor input2 = make_tensor<float>(vec, {2, 2});
   std::vector<Tensor> in{input1, input2};
   std::vector<Tensor> output{input1};
-  EXPECT_NO_THROW(auto ret1 = layer.getLegacyImplBool());
-  EXPECT_NO_THROW(auto ret2 = layer.getNumericParams());
-  EXPECT_NO_THROW(auto ret3 = layer.getTensorParams());
+  
+  bool is_legacy = layer.getLegacyImplBool();
+  std::vector<size_t> nums = layer.getNumericParams();
+  auto tens = layer.getTensorParams();
+  EXPECT_EQ(is_legacy, false);
+  EXPECT_FALSE(nums.empty());
+  EXPECT_FALSE(tens.empty());
 }
 
 TEST(ConvolutionalLayerTest, IncompatibleInput) {
